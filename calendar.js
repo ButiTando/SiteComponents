489ebb54e866528741f4d8481039d1302e7a00a1
Tando Mahlati <116363298+ButiTando@users.noreply.github.com>
2023-10-16 19:50:55 +0000

    Current updates

---
+++ calendar.js
@@ -0,0 +1,20 @@
+const calendarLeftArrow = document.getElementById('calendar-left-arrow');
+const calendarRightArrow = document.getElementById('calendar-right-arrow');
+const calendarMonthLable = document.getElementById('calendar-month');
+
+var monthTracker = 0;
+
+calendarLeftArrow.addEventListener('click',()=>{
+
+    console.log("The left arrow key was pressed");
+    monthTracker ++;
+    calendarMonthLable.innerText = `Month ${monthTracker}`;
+    
+})
+
+calendarRightArrow.addEventListener('click',()=>{
+    console.log("The right arrow key was pressed");
+    monthTracker --;
+    calendarMonthLable.innerText = `Month ${monthTracker}`;
+    
+})
