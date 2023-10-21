84ea34d6ac901afa9b40c9a0cec531966caabf2b
Tando Mahlati <116363298+ButiTando@users.noreply.github.com>
2023-10-21 22:38:22 +0000

    Current update

--- calendar.js
+++ calendar.js
@@ -1,8 +1,12 @@
 const calendarLeftArrow = document.getElementById('calendar-left-arrow');
 const calendarRightArrow = document.getElementById('calendar-right-arrow');
 const calendarMonthLable = document.getElementById('calendar-month');
-var calendarDays = document.getElementsByClassName('calender-day');
 
+var calendar = document.getElementById("calendar");
+var calendarHeader = document.getElementById("calendar-heading");
+var calendarDays = document.getElementsByClassName('calendar-day');
+var calendarWeeks = document.getElementsByClassName('calendar-week');
+var calendarContainer = document.getElementById('calendar-container');
 
 const strMonths = [ "January",
                     "February",
@@ -37,9 +41,52 @@ function populateDays(currentMonth, year){
     });
 };
 
+function aduptCalendar(){
+
+    // Calendar dimension    
+    calendarWidth = calendarContainer.clientWidth;
+    calendarHeight = calendarContainer.clientHeight;
+
+    // Calendar heading dimensions
+    var calendarHeadingHeight = calendar.clientHeight * 0.1;
+    var calendarHeadingWidth = calendar.clientWidth * 1;
+
+    // Calendar container dimensions
+
+    var calendarContainerHeight = calendar.clientHeight - calendarHeadingHeight;
+    var calendarContainerWidth = calendar.clientWidth * 1;
+
+    calendarHeader.style.height = `${calendarHeadingHeight}px`;
+    calendarHeader.style.lineHeight = `${calendarHeadingHeight}px`;
+    calendarHeader.style.width = `${calendarHeadingWidth}px`;
+
+    calendarContainer.style.height = `${calendarContainerHeight}px`;
+    calendarContainer.style.width = `${calendarContainerWidth}px`;
+
+    var dayWidth = calendarWidth / 7;
+    var weekHeight = calendarHeight / 5;
+
+    // Converting the calendar day elements into array for iteration
+    Array.from(calendarDays).forEach((day)=>{
+        day.style.width = `${dayWidth}px`;
+    });
+
+    // Converting the calendar week elements into array for iteration
+    Array.from(calendarWeeks).forEach((week)=>{
+        week.style.height = `${weekHeight}px`;
+    });
+}
+
 window.onload = () => {
+    aduptCalendar();
     populateDays(intMonth, currentDate.getFullYear());
     calendarMonthLable.innerText = `${strMonths[intMonth]}`;
+};
+
+// Updating calendar
+
+calendar.onchange = () => {
+    aduptCalendar();
 };
 
 calendarLeftArrow.addEventListener('click',()=>{
